13da583ba16a0155e75ca4a2c2831fbbad3d1559
Max Yakushev <m.yakushev23@gmail.com>
2023-07-15 19:37:53 +0300

    fix: styles

--- src/shared/ui/error-message.tsx
+++ src/shared/ui/error-message.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from 'react'
 
 export const ErrorMessage: FC<{ children: ReactNode }> = ({ children }) => {
-  return <div className='text-[12px] absolute top-[52px] text-error w-full font-medium text-right'>{children}</div>
+  return <div className='text-[11px] absolute top-[52px] text-error w-full font-medium text-right'>{children}</div>
 }
